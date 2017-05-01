fdea5a7ec03e2d74f45b58dc2998a240baec6976
Lorenzi <galantel1@hawkmail.newpaltz.edu>
2017-05-01 14:36:17 -0400

    Updated StudentClasses.java

--- app/src/main/java/com/example/lorenzi/krokin/StudentClasses.java
+++ app/src/main/java/com/example/lorenzi/krokin/StudentClasses.java
@@ -6,6 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class StudentClasses extends AppCompatActivity {
 
@@ -17,4 +20,14 @@ public class StudentClasses extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
     }
+
+    Button mAdd = (Button) findViewById(R.id.add);
+    Button mDrop = (Button) findViewById(R.id.drop);
+    Button mEdit = (Button) findViewById(R.id.edit);
+
+    ArrayList<Class> studentClassList = new ArrayList<Class>();
+    studentClassList.add(new Class("Software Engineering", "5.00-6.15P MW","N/A","HUM218", "Di Wang"));
+
+
+
 }
