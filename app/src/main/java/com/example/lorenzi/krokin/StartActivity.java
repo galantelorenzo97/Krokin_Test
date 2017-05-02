4c27779eda91f055757e2057ad43d852f2aa050f
brownk11 <kwamebrown58@gmail.com>
2017-05-01 19:44:35 -0700

    Update

---
+++ app/src/main/java/com/example/lorenzi/krokin/StartActivity.java
@@ -0,0 +1,53 @@
+package com.example.lorenzi.krokin;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onCreate2() {
+
+
+        Button button = (Button) findViewById(R.id.login);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+
+            public void onClick(View view) {
+
+                Toast.makeText(StartActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+
+}
+
--- app/src/main/java/com/example/lorenzi/krokin/StudentClasses.java
+++ app/src/main/java/com/example/lorenzi/krokin/StudentClasses.java
@@ -24,7 +24,7 @@ public class StudentClasses extends AppCompatActivity {
             public void onClick(View v) {
 
             }
-        }
+        });
     }
 
     Button mAdd = (Button) findViewById(R.id.add);
