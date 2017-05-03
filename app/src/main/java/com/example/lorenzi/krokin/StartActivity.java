1e04e74e9fbde1b2c9d2a172073ba056c14acfc2
Lorenzi <galantel1@hawkmail.newpaltz.edu>
2017-05-03 13:25:01 -0400

    onClick removed from XML and implemented in Java. Message dialog created.

--- app/src/main/java/com/example/lorenzi/krokin/StartActivity.java
+++ app/src/main/java/com/example/lorenzi/krokin/StartActivity.java
@@ -2,6 +2,8 @@ package com.example.lorenzi.krokin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,6 +11,14 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start2);
+
+        Button mLoginCtrl = (Button) findViewById(R.id.login_btn);
+        mLoginCtrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 
